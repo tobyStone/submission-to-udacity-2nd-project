9976a545f030325804b50ab4bab6aad83994a497
stickmonster <40922682+stickmonster@users.noreply.github.com>
2020-05-27 17:47:42 +0100

    Add files via upload

---
+++ include/process.h
@@ -0,0 +1,45 @@
+
+
+#ifndef PROCESS_H
+#define PROCESS_H
+
+#include <string>
+/*
+Basic class for Process representation
+It contains relevant attributes as shown below
+*/
+class Process {
+public:
+    Process(const int, int cpuUsage_);
+    int Pid();                               // TODO: See src/process.cpp
+    std::string User();                      // TODO: See src/process.cpp
+    std::string Command();                   // TODO: See src/process.cpp
+    int CpuUtilization();                  // TODO: See src/process.cpp
+    std::string Ram();                       // TODO: See src/process.cpp
+    long int UpTime();                       // TODO: See src/process.cpp
+    bool operator<(Process const& a) const;  // TODO: See src/process.cpp
+
+    // TODO: Declare any necessary private members
+private:
+    int pid_ = 0;
+    std::string user_ = " ";
+    std::string command_ = " ";
+    int cpuUsage_ = 0;
+    std::string ram_ = " ";
+    long int uptime_ = 0.0;
+    enum processElements {
+        utime_ = 0,
+        stime_,
+        cutime_,
+        cstime_,
+        starttime_
+    };
+    float futime_ = 0.0;
+    float fstime_ = 0.0;
+    float fcutime_ = 0.0;
+    float fcstime_ = 0.0;
+    float fstarttime_ = 0.0;
+
+};
+
+#endif
