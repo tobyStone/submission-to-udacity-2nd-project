6cc08b146a169d31d3fa703f8d63c93b82f5f112
stickmonster <40922682+stickmonster@users.noreply.github.com>
2020-05-31 15:29:14 +0100

    Update process.cpp

--- src/process.cpp
+++ src/process.cpp
@@ -64,7 +64,7 @@ string Process::Ram() { return LinuxParser::Ram(pid_); }
 string Process::User() { return LinuxParser::User(pid_); }
 
 // TODO: Return the age of this process (in seconds)
-long int Process::UpTime() { return LinuxParser::UpTime(); }
+long int Process::UpTime() { return LinuxParser::UpTime(pid_); }
 
 // TODO: Overload the "less than" comparison operator for Process objects
 // REMOVE: [[maybe_unused]] once you define the function
